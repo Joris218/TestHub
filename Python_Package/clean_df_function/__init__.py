fccce2c72dda5c0e9cd6348f3f0453d668bc53ea
Joris <55034585+Joris218@users.noreply.github.com>
2024-03-15 10:01:26 +0100

    Delete Python_Package directory

--- Python_Package/clean_df_function/__init__.py
+++
@@ -1,2 +0,0 @@
-def init():
-    print("The 'clean_df_function.py' initialization was successful")
--- Python_Package/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='clean_df_function',
-    version='0.0.1',
-    author='jwolbrink',
-    packages=find_packages(),
-)
